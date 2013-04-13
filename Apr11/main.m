e941c1a579c45cbc300d243309a50bda109fe3ad
chrisdiluccia <cdiluccia@gmail.com>
2013-04-12 21:19:10 -0400

    initial commit

---
+++ Apr11/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Apr11
+//
+//  Created by Christopher J Di Luccia on 4/12/13.
+//  Copyright (c) 2013 Christopher J Di Luccia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Apr11AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Apr11AppDelegate class]));
+    }
+}
